209b6db1e5f13d8c1898b5b627354b6c08281c61
py-ovarion <olegk6783@gmail.com>
2026-01-13 08:43:48 +0200

    add verification **kwargs arguments

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args) -> Any:
-        if args in cache_dict:
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, frozenset(kwargs.items()))
+        if key in cache_dict:
             print("Getting from cache")
         else:
             print("Calculating new result")
-            cache_dict[args] = func(*args)
-        return cache_dict[args]
+            cache_dict[key] = func(*args, **kwargs)
+        return cache_dict[key]
     return wrapper
